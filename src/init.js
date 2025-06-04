fa7a72f61ca6674c723d99aa580317cf7aee30ba
TiAmo <576193486@qq.com>
2025-06-04 15:26:24 +0800

    处理新版和旧版加载index.js的逻辑

--- src/init.js
+++ src/init.js
@@ -1,10 +1,16 @@
 require("./liteloader_api/main.js");
 require("./loader_core/plugin_loader.js");
 require("./main.js");
-require(require("path").join(process.resourcesPath, "app/app_launcher/index.js"));
+const version = LiteLoader.package.qqnt.buildVersion;
+
+if (version < 34467){
+    require(require("path").join(process.resourcesPath, "app/app_launcher/index.js"));
+}
+else {
+    require(require("path").join(process.resourcesPath, "app/application.asar/app_launcher/index.js"));
+}
 
 setImmediate(() => {
-    const version = LiteLoader.package.qqnt.buildVersion;
     global.launcher.installPathPkgJson.main = (() => {
         if (version >= 29271) return "./application.asar/app_launcher/index.js";
         if (version >= 28060) return "./application/app_launcher/index.js";
